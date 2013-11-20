bd2bf3e9d797d863229717a1153e8f96f7347799
bill-march <march@wlan-rooms-952.sc13.org>
2013-11-20 08:49:54 -0700

    Added KL divergence class

--- mlpack_code/KLDivergence.hpp
+++ mlpack_code/KLDivergence.hpp
@@ -2,3 +2,32 @@
  *  Implments the KL divergence 
  */
 
+namespace bbtree {
+
+  /**
+   * For now we assume that the distribution is discrete, and represented by a vector of doubles.
+   * This means that the user who instantiates this as TBregmanDiv needs to instantiate 
+   * TDataType as std::vector<double>
+   */
+  class KLDivergence
+  {
+    
+  private:
+    
+    
+    
+    
+  public:
+    
+    static double Divergence(std::vector<double>& x, std::vector<double>& y);
+    
+    static std::vector<double> Gradient(std::vector<double>& x);
+  
+    static std::vector<double> GradientConjugate(std::vector<double>& x);
+  
+    
+    
+  }; 
+
+}
+
